a23be61f29297ddfbc46b8e87d3f950105158088
yunisuthn <yunis.thn@gmail.com>
2022-06-08 15:54:14 +0300

    UPDATE adminGlobal vue OK

--- Models/CoursModel.js
+++ Models/CoursModel.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const User = mongoose.Schema({
     name_Cours: String,
     date_Commenc: Date,
-    nbParticp: Number,
+    //nbParticp: Number,
     professeur: String,
     type: String,
-    niveau: String,
-    groupe: String,
-    participant: String
+    //niveau: String,
+    //groupe: String,
+    //participant: String
 })
 
 module.exports = mongoose.model('dataCours', User);
