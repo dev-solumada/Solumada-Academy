99152441909b4e127ded3ea73922a2c9e24b3d32
Thierry Keller <thierry0keller@gmail.com>
2022-06-10 15:14:01 +0300

    new tst

--- Vue/assets/basetemplate/assets/js/users.js
+++ Vue/assets/basetemplate/assets/js/users.js
@@ -62,10 +62,10 @@ $('#saveUser').on("click", function()
                     {
                     confirmButtonText: 'Ok',
                 }).then((result) => {
+                    $('#closeModal').click();
                     if (result.isConfirmed) {
                         userDatatable.ajax.reload(null, false);
                         userDatatable.page(currentPage).draw('page');
-                        $('#closeModal').click();
                     }
                 })
             }
@@ -120,6 +120,7 @@ $(document).on('click', '#saveUpdateUser', function(){
                 $('#errorUpdateUser').css('display', 'block');
                 $('#errorUpdateUser').html('<strong>'+response+'</strong>' + ': email or username already taken');
             } else {
+                $('#closeModalUpdate').click();
                 resetForm(action='update');
                 responsetxt = response + ' Updated successfully';
                 Swal.fire(
@@ -131,7 +132,6 @@ $(document).on('click', '#saveUpdateUser', function(){
                   }).then((result) => {
                     if (result.isConfirmed) {
                         userDatatable.ajax.reload(null, false);
-                        $('#closeModal').click();
                         userDatatable.page(currentPage).draw('page');
                     }
                 })
@@ -180,7 +180,6 @@ $(document).on('click', '.btnDeleteUser', function()
                                             timer: 1600
                                         });
                                         userDatatable.ajax.reload(null, false);
-                                        $('#closeModal').click();
                                         userDatatable.page(currentPage).draw('page');
                                     },
                                     error: function(response){
@@ -206,114 +205,6 @@ $(document).on('click', '.btnDeleteUser', function()
         }
     )
 });
-
-
-
-
-// function getuser(id)
-// {
-
-//     $.ajax(
-//         {
-//             url : "/getuser",
-//             method: 'post',
-//             dataType: 'json',
-//             data: {id: id},
-//             success: function(user){
-//                     $('#user_id').val(user._id);
-//                     $('#name_update').val(user.name);
-//                     $('#email_update').val(user.username);
-//                     $('#m_code_update').val(user.m_code);
-//                     $('#num_agent_update').val(user.num_agent);
-//                     $('#type_util_update').val(user.type_util);
-//                 },
-//             error: function(err){
-//                     alert(JSON.stringify(err));
-//             }
-//         }
-//     )
-// }
-
-
-// function addUser(url)
-// {
-//     formAddData = {
-//                     name: $('#name').val(),
-//                     email: $('#email').val(),
-//                     m_code: $('#m_code').val(),
-//                     num_agent: $('#num_agent').val(),
-//                     type_util: $('#type_util').val()
-//                 }
-
-//     $.ajax({
-//         url: url,
-//         method: 'post',
-//         data: formAddData,
-//         success: function(response){
-//             if(response == 'error'){
-//                 $('#successAddUser').css('display', 'none');
-//                 $('#errorAddUser').css('display', 'block');
-//                 $('#errorAddUser').html('<strong>'+response+'</strong>' + ': email or username already taken');
-//             } else {
-//                 resetForm(action='add');
-//                 responsetxt = response + ' Saved successfully';
-//                 Swal.fire(
-//                     'User Saved',
-//                     responsetxt,
-//                     'success',
-//                     {
-//                     confirmButtonText: 'Ok',
-//                   }).then((result) => {
-//                     if (result.isConfirmed) {
-//                     window.location = "/listeUser";
-//                     }
-//                   })
-//             }
-//         }
-//     });
-// }
-
-function updateUser(url)
-{
-    formUpdateData = {
-        id : $('#user_id').val(),
-        username: $('#name_update').val(),
-        email: $('#email_update').val(),
-        m_code: $('#m_code_update').val(),
-        num_agent: $('#num_agent_update').val(),
-        type_util: $('#type_util_update').val()
-    }
-
-    $.ajax({
-        url: url,
-        method: 'post',
-        data : formUpdateData,
-        success : function(response){
-            if(response == 'error'){
-                $('#errorUpdateUser').css('display', 'block');
-                $('#errorUpdateUser').html('<strong>'+response+'</strong>' + ': email or username already taken');
-            } else {
-                resetForm(action='update');
-                responsetxt = response + ' Updated successfully';
-                Swal.fire(
-                    'User Updated',
-                    responsetxt,
-                    'success',
-                    {
-                    confirmButtonText: 'Ok',
-                  }).then((result) => {
-                    if (result.isConfirmed) {
-                    window.location = "/listeUser";
-                    }
-                })
-            }
-        },
-        error: function(response){
-            alert(JSON.stringify(response));
-        }
-    })
-}
-
 
 function getuserAndDelete(id)
 {
