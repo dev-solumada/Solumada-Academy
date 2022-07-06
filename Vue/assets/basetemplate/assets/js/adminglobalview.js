1dc1b0e52e226ac4b7366272dac0e47939e1fa26
thierryKeller-1 <thierry0keller@gmail.com>
2022-07-06 08:40:46 +0300

    datatable ajax done

---
+++ Vue/assets/basetemplate/assets/js/adminglobalview.js
@@ -0,0 +1,45 @@
+let AdminglobalViewDatatable = $('#AdminglobalViewDatatable').DataTable(
+    {
+      ajax:     {
+                      'url': '/adminGlobalViewAjax',
+                      'dataSrc': '',
+                  },
+      columns: [
+                      {'data': 'emp_email'},
+                      {'data': 'emp_m_code'},
+                      {'data': 'emp_number'},
+                      {'data': 'emp_courslevel', 'render': function(emp_courslevel){
+                        var options = "";
+                        emp_courslevel.forEach(cours => {
+                            options = options + `<option value=${cours} class='text-center'>${cours}</option>`;
+                        });
+                        var coursLevels = `<div class='input-group'><select class='form-control selectNiveau'>${options}</select></div>`;
+                        return coursLevels;
+                      }},
+                      {'data': 'emp_point', 'render': function(emp_point){
+                        var points = "";
+                        var user_point = `<option value=${emp_point[0]} class='text-center' selected>${emp_point[0]}</option>`;
+                        points = points + user_point;
+                        delete emp_point[0];
+                        emp_point.forEach(point => {
+                          points = points + `<option value=${point} class='text-center'>${point}</option>`;
+                        });
+                        var points_data = `<div class='input-group'><select class='form-control selectNiveau'>${points}</select></div>`;
+                        return points_data;
+                      }},
+                      {'data': 'emp_grade', 'render': function(emp_grade){
+                        var grades = ""
+                        var user_grade = `<option value=${emp_grade[0]} class='text-center' selected>${emp_grade[0]}</option>`;
+                        grades = grades + user_grade;
+                        delete emp_grade[0];
+                        emp_grade.forEach(grade => {
+                          grades = grades + `<option value=${grade} class='text-center'>${grade}</option>`;
+                        });
+                        var grades_data = `<div class='input-group'><select class='form-control selectNiveau'>${grades}</select></div>`;
+                        return grades_data;
+                      }}
+
+
+                  ],
+    }
+);
