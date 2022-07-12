638e0d0b29084d503d67e65a12c0503eeb7f787f
yunisuthn <yunis.thn@gmail.com>
2022-07-12 10:01:01 +0300

    update liste Cours admin

--- Vue/assets/js/ajax/liste-cours.js
+++ Vue/assets/js/ajax/liste-cours.js
@@ -3,43 +3,29 @@ getGroupeList()
 var currentUrl = window.location.href;
 var arg = currentUrl.split('/');
 arg = $(arg).get(-1);
-console.log("arg ", arg);
-var tableGroupAdmin = $('#tableGroupAdmin').DataTable(
-    {
-        "ajax": { "url": `/teacherParcours/${arg}`, "dataSrc": "" },
-        "columns": [
-            {'data': 'date'},
-            {'data': 'start_time'},
-            {'data': 'end_time'},
-            {'data': 'group_name'},
-            {'data': 'present', 'render': function(present){
-                        var presenceOptionData = '';
-                        present.forEach(element => {
-                            presenceOptionData = presenceOptionData + `<option>${element}</option>`
-                        });
-                        presentOptions = `<select data-placeholder="" class="prensentSelect form-control" tabindex="1">${presenceOptionData}</select>`;
-                        return presentOptions;
-                    }
-        },
-            {'data': 'absent', 'render': function(absent){
-                var absentOptionData = '';
-                absent.forEach(element => {
-                    absentOptionData = absentOptionData + `<option>${element}</option>`
-                });
-                absentOptions = `<select data-placeholder="" class="prensentSelect form-control" tabindex="1">${absentOptionData}</select>`;
-                return absentOptions;
-            }
-        },
-            {"defaultContent": "\
-                                <div class='btn-group d-flex justify-content-center' role='group' aria-label='Basic mixed styles example'>\
-                                    <button type='button'  class='btn px-2 btn-sm btn-warning btnUpdateCours' type='button' class='btn btn-sm btn-warning' data-toggle='modal' data-target='#updateCours' data-bs-whatever='@getbootstrap'><i class='fa fa-edit'></i></button>\
-                                    <button type='button'  class='btn px-2 btn-sm btn-danger btnDeleteCours' type='button' class='btn btn-sm btn-warning'><i class='fa fa-trash'></i></button>\
-                                </div>\
-                                "
-            }
-        ]
-    }
-)
+
+var select_gp = document.getElementById("select-group")
+console.log("onchange", select_gp);
+var groupe = "GROUPE FIRST"
+
+console.log("groupe ", groupe);
+
+function functTabl() {
+    setTimeout( 
+        $('#tableGroupAdmin').DataTable(
+            {
+                "ajax": { "url": `/groupe/${arg}/${groupe}`, "dataSrc": "" },
+                "columns": [
+                    {'data': 'username'},
+                    {'data': 'mcode'},
+                    {'data': 'num_agent'}
+
+                ]}) 
+                , 10000)
+    
+}
+
+functTabl()
 $("#saveGroupe").on("click", function()
 {
     GroupData = { newgroupe: $('#groupeNew').val(), cours: $('#cours').val()}
@@ -69,6 +55,76 @@ $("#saveGroupe").on("click", function()
           }
         }
     });
+}
+);
+
+$("#select-group").on("change", function()
+{
+    if (select_gp != "") {
+            
+        $("#tableGroupAdmin").DataTable().ajax.reload(null, false);
+
+                //     {'data': 'present', 'render': function(present){
+                //                 var presenceOptionData = '';
+                //                 present.forEach(element => {
+                //                     presenceOptionData = presenceOptionData + `<option>${element}</option>`
+                //                 });
+                //                 presentOptions = `<select data-placeholder="" class="prensentSelect form-control" tabindex="1">${presenceOptionData}</select>`;
+                //                 return presentOptions;
+                //             }
+                // },
+                //     {'data': 'absent', 'render': function(absent){
+                //         var absentOptionData = '';
+                //         absent.forEach(element => {
+                //             absentOptionData = absentOptionData + `<option>${element}</option>`
+                //         });
+                //         absentOptions = `<select data-placeholder="" class="prensentSelect form-control" tabindex="1">${absentOptionData}</select>`;
+                //         return absentOptions;
+                //     }
+                // },
+                //     {"defaultContent": "\
+                //                         <div class='btn-group d-flex justify-content-center' role='group' aria-label='Basic mixed styles example'>\
+                //                             <button type='button'  class='btn px-2 btn-sm btn-warning btnUpdateCours' type='button' class='btn btn-sm btn-warning' data-toggle='modal' data-target='#updateCours' data-bs-whatever='@getbootstrap'><i class='fa fa-edit'></i></button>\
+                //                             <button type='button'  class='btn px-2 btn-sm btn-danger btnDeleteCours' type='button' class='btn btn-sm btn-warning'><i class='fa fa-trash'></i></button>\
+                //                         </div>\
+                //                         "
+                //     }
+                // ]
+
+
+    }
+
+
+
+
+
+    // GroupData = { newgroupe: $('#groupeNew').val(), cours: $('#cours').val()}
+
+    // console.log("save goupre");
+    //         $("#select-gpe").append(new Option("Hello", "1"));
+    // $.ajax({
+    //     url: '/addgroupe',
+    //     method: 'post',
+    //     data: GroupData,
+    //     success: function(response){
+    //       if(response == "error")
+    //       {
+    //         Swal.fire({
+    //           icon: 'error',
+    //           title: 'Error',
+    //           text: 'this groupe is already exist!',
+    //         });
+    //       }else{
+    //         Swal.fire({
+    //           icon: 'success',
+    //           title: 'New Point Saved',
+    //           text: `Point ${GroupData.newgroupe} saved successfully`,
+    //         });
+    //         clearForm();
+    //         getGroupeList()
+    //       }
+    //     }
+    // });
 }
 );
 
@@ -104,6 +160,9 @@ function getGroupeList()
             success: function(data1)
                 {
                     $('#select-group').empty();
+                    // $("#select-group").append(`<option value="">
+                    // Choose groupe
+                    // </option>`);
                     $.each(data1, function(key, value)
                     {
                         $("#select-group").append(`<option value="${data1[key].name_Groupe}">
