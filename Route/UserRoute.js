c073466c3ed2503122c88ac3f6e06c834bc6975c
yunisuthn <yunis.thn@gmail.com>
2022-07-13 15:48:10 +0300

    update time table admin

--- Route/UserRoute.js
+++ Route/UserRoute.js
@@ -514,7 +514,6 @@ routeExp.route("/teacherTimeTable/:cours").get(async function (req, res) {
             )
             .then(async () => {
                 var time = await EmplTemp.find({ cours: cours });
-                console.log("time ", time);
                 res.send(JSON.stringify(time));
             });
     }
@@ -540,7 +539,7 @@ class Parcours {
 routeExp.route("/teacherParcours/:cours").get(async function (req, res) {
     var session = req.session;
     var cours = req.params.cours;
-    if (session.occupation_prof == "Professeur") {
+    if (session.occupation_prof == "Professeur" || session.occupation_adm == "adm") {
         mongoose
             .connect(
                 "mongodb+srv://solumada-academy:academy123456@cluster0.xep87.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
