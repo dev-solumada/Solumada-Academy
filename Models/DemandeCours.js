23e37eea54345b2257caaaf94adcc1cb9e87420b
yunisuthn <yunis.thn@gmail.com>
2022-07-21 20:03:50 +0300

    find the bug

--- Models/DemandeCours.js
+++ Models/DemandeCours.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const DemandCours = mongoose.Schema({
-    cours: String,
+    coursd: String,
     user: String,
     demand: Boolean
 })
 
-module.exports = mongoose.model('dataCoursDemander', DemandCours);
+module.exports = mongoose.model('coursDemand', DemandCours);
